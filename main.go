0fd9d023467a2e8c698239b4f7ef6116094cd4f3
7minutech <elijohnson1107@gmail.com>
2025-10-20 17:11:29 -0500

    feat: middlewareMetricsInc tracks request to file server

--- main.go
+++ main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	var apiCfg = apiConfig{}
 
+	mux := http.NewServeMux()
+
+	handlerFile := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handlerFile))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -26,4 +35,9 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return next
 }
