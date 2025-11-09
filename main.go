f985aca0e46dae41ce9b87ccfb46194f539525bd
7minutech <elijohnson1107@gmail.com>
2025-11-08 18:46:17 -0600

    fix: typo in User fields

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ type apiConfig struct {
 
 type User struct {
 	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"create_at"`
-	UpdatedAt time.Time `json:"update_at"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 	Email     string    `json:"email"`
 }
 
